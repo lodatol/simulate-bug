d2229c2c14dba02ca7927c7ea569a6eaffdac629
Vincenzo Ferrari <wilk3ert@gmail.com>
2016-10-20 17:21:18 +0200

    from es5 to es6
    because style matters

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-var express = require('express');
-var app = express();
-var process = require('process');
-app.get('/', function (req, res) {
-	if (Math.random()<parseFloat(process.env.PBUG))
-	{
-		res.send('Bug found');
-		process.exit(-1);
-	}
-	res.send('No bug!');
-});
+const express = require('express'),
+      app = express(),
+      process = require('process')
 
-app.listen(8888, function () {
-	console.log('Example app listening on port 3000!');
-});
+app.get('/', (req, res) => {
+	if (Math.random() < parseFloat(process.env.PBUG)) {
+		res.send('Bug found')
+		process.exit(-1)
+		return
+	}
+	
+	res.send('No bug!')
+})
+
+app.listen(8888, () => {
+	console.log('Example app listening on port 3000!')
+})
 
