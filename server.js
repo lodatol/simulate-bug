2a8eaa12fe2ecb7c8a35c25ea624ba8b7003bdf4
Lodato Luciano <lodato@ibuildings.it>
2016-10-20 09:07:39 +0200

    second commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var process = require('process');
+app.get('/', function (req, res) {
+	if (Math.random()<parseFloat(process.env.PBUG))
+	{
+		res.send('Bug found');
+		process.exit(-1);
+	}
+	res.send('No bug!');
+});
+
+app.listen(8888, function () {
+	console.log('Example app listening on port 3000!');
+});
+
+console.log('Running on http://localhost:' + config.port);
